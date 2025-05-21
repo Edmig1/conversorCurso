017f9ba0b28e1240aaead1c5671632c1bd0408cf
Miguel Garrocini <miguelitoplayer1@gmail.com>
2025-05-21 11:26:06 -0300

    função de converter celsius feita

--- conversor.py
+++ conversor.py
@@ -1,5 +1,11 @@
 def convertekm():
     kmh = int(input("Qual a velocidade em Km/h: "))
     mps = kmh / 3.6
-    print(mps)
+    print(f'A velocidade em m/s é: {mps} ')
 convertekm()
+
+def converteCelsius():
+    cel = int(input("Qual a velocidade em Km/h: "))
+    far = (cel*1.8)+32
+    print(f'a temperatura em Farenheit é: {far}')
+converteCelsius()
